29efb30d69d07d94a56a807f2e26bebd9d68ff18
JhoimarSilva <jhoimar.silva@correounivalle.edu.co>
2023-10-04 23:54:05 -0500

    Actualizacion

--- ejercicio1.rkt
+++ ejercicio1.rkt
@@ -1,64 +1,66 @@
 #lang eopl
 #|
- Estiven Andrés Martínez Granados:2179687
- Jhoimar Silva Torres:2177167
- Ervin Caravali Ibarra:1925648
+Estiven Andrés Martínez Granados:2179687
+Jhoimar Silva Torres:2177167
+Ervin Caravalí Ibarra:1925648
 |#
-
-<expr> ::= (fnc <var> <clausulas>)
-<var> ::= <num>
-<clausulas> ::= ' ( <clausula-list> )
-<clausula-list> ::= <clausula> <clausula-list> | ε
-<clausula> ::= ( <literal-list> )
-<literal-list> ::= <literal> <literal-list> | ε
-<literal> ::= <num> | -<num>
-<num> ::= [0-9]+
-
-
-
-;;Implementación basada en listas
+;; Implementación basada en listas
 (define (fnc var clausulas)
-(list 'fnc var clausulas))
+  (list 'fnc var clausulas))
 
 (define (fnc-var expr)
-(cadr expr))
+  (cadr expr))
 
 (define (fnc-clausulas expr)
-(caddr expr))
+  (caddr expr))
 
 (define (or-vars expr)
-(cond
-[(list? expr) (cdr expr)]
-[else (list expr)]))
+  (cond
+    [(list? expr) (cdr expr)]
+    [else (list expr)]))
 
 (define (and cl1 cl2)
-(list 'and cl1 cl2))
+  (list 'and cl1 cl2))
 
 (define e1 (fnc 4 '((1 2 3 4)
-(and (or -1 -2) (or 3))
-(and (or -2 -3 -4)))))
+                   (and (or -1 -2) (or 3))
+                   (and (or -2 -3 -4)))))
 
 (define e2 (fnc 3 '((1 -2)
-(and (or 2) (or -1)))))
+                   (and (or 2) (or -1)))))
 
 (define e3 (fnc 2 '((1))))
 
-;;Casos de prueba
 
-(fnc-var (fnc 4 '((1 2 3 4)
-(and (or -1 -2) (or 3)))));;Resultado esperado 4
 
-(fnc-clausulas (fnc 4 '((1 2 3 4)
-(and (or -1 -2) (or 3)))));;Resultado esperado ((1 2 3 4)(and (or -1 -2) (or 3)))
+;; Casos de prueba
 
-(or-vars (or -1 -2));;Resultado esperado (-1)
+(display (fnc-var (fnc 4 '((1 2 3 4)
+                           (and (or -1 -2) (or 3))
+                           (and (or -2 -3 -4)))))) ;; Resultado esperado 4
 
-(or-vars (or -2 -2 -2));;Resultado esperado (-2)
+(newline)
 
-(fnc-var (fnc 1 '((4 3 2 1)
-(and (or -2 -1) (or 1)))));;Resultado esperado 1
+(display (fnc-clausulas (fnc 4 '((1 2 3 4)
+                                  (and (or -1 -2) (or 3))
+                                  (and (or -2 -3 -4)))))) ;; Resultado esperado ((1 2 3 4) (and (or -1 -2) (or 3)) (and (or -2 -3 -4)))
 
-;;----------------------------------------------------------------------------------------
+(newline)
+
+(display (or-vars (or -1 -2))) ;; Resultado esperado (-1)
+
+(newline)
+
+(display (or-vars (or -2 -2 -2))) ;; Resultado esperado (-2)
+
+(newline)
+
+(display (fnc-var (fnc 1 '((4 3 2 1)
+                           (and (or -2 -1) (or 1)))))) ;; Resultado esperado 1
+(newline)
+
+
+;;------------------------------------------------ ----------------------------------------
 
 ;; Implementación basada en datatypes
 
@@ -95,51 +97,10 @@
 (define instance2 '(FNC 3 ((-1 2) (3) (-2 4))))
 (define instance3 '(FNC 2 ((1 -2) (-1 -2) (2))))
 
+;; Ejemplos de instancias SAT en el formato basado en datatypes
+(define parsed-instance1 (make-fnc 4 '((1 -2 3 4) (-2 3) (-1 -2 -3) (3 4) (2))))
+(define parsed-instance2 (make-fnc 3 '((-1 2) (3) (-2 4))))
+(define parsed-instance3 (make-fnc 2 '((1 -2) (-1 -2) (2))))
 
-
-
-;;Parseo de las instancias SAT
-(define parsed-instance1 (parse-fnc instance1))
-(define parsed-instance2 (parse-fnc instance2))
-(define parsed-instance3 (parse-fnc instance3))
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-;; Definición del datatype para <expr>
-(define-datatype expr expr?
-  (fnc (var : var?) (clausulas : clausulas?)))
-
-;; Definición del datatype para <var>
-(define-datatype var var?
-  (var (num : num?)))
-
-;; Definición del datatype para <clausulas>
-(define-datatype clausulas clausulas?
-  (clausulas (clausula-list : clausula-list?)))
-
-;; Definición del datatype para <clausula-list>
-(define-datatype clausula-list clausula-list?
-  (cons-clausula (clausula : clausula?) (clausula-list : clausula-list?))
-  (empty-clausula-list))
-
-;; Definición del datatype para <clausula>
-(define-datatype clausula clausula?
-  (clausula (literal-list : literal-list?)))
-
-;; Definición del datatype para <literal-list>
-(define-datatype literal-list literal-list?
-  (cons-literal (literal : literal?) (literal-list : literal-list?))
-  (empty-literal-list))
-
-;; Definición del datatype para <literal>
-(define-datatype literal literal?
-  (num (num : num?))   ; Representa un número entero.
-  (neg-num (num : num?))   ; Representa un número entero negativo.
-
-;; Definición del datatype para <num>
-(define-datatype num num?
-  (num (val : integer?)))   ; Representa un número entero.
 
 
--- ejercicio3.rkt
+++ ejercicio3.rkt
@@ -1,6 +1,51 @@
+
 #lang eopl
+(require "ejercicio1.rkt")
+
 #|
  Estiven Andrés Martínez Granados:2179687
  Jhoimar Silva Torres:2177167
  Ervin Caravali Ibarra:1925648
 |#
+
+(define (EVALUARSAT instancia asignacion)
+  (define (evaluar-clausula clausula asignacion)
+    (cond
+      [(null? clausula) #t] ; Clausula vacía es verdadera
+      [(member (car clausula) asignacion) #t]
+      [else (evaluar-clausula (cdr clausula) asignacion)])) ; Evaluación recursiva
+
+  (define (evaluar-fnc fnc asignacion)
+    (cond
+      [(null? fnc) #t] ; Todas las cláusulas son verdaderas
+      [(evaluar-clausula (car fnc) asignacion)
+       (evaluar-fnc (cdr fnc) asignacion)]
+      [else #f])) ; Al menos una cláusula es falsa
+
+  (define (evaluar-instancia instancia asignacion)
+    (let ((clausulas (if (list? instancia) (fnc-clausulas instancia) (make-fnc instancia))))
+      (evaluar-fnc clausulas asignacion)))
+
+  (let ((resultado (evaluar-instancia instancia asignacion)))
+    (if resultado
+        (begin
+          (display "Satisfactible")
+          (list 'respuesta resultado))
+        (begin
+          (display "Insatisfactible")
+          (list 'respuesta resultado)))))
+
+
+;;Casos prueba listas:
+(display (EVALUARSAT e1 '(1 3 4))) ; Debe mostrar "Satisfactible"
+(display (EVALUARSAT e1 '(1 -3 4))) ; Debe mostrar "Satisfactible"
+(display (EVALUARSAT e2 '(-1 2))) ; Debe mostrar "Satisfactible"
+(display (EVALUARSAT e3 '(1))) ; Debe mostrar "Insatisfactible"
+(newline)
+
+;;Casos prueba datatypes:
+(newline)
+(display (EVALUARSAT parsed-instance1 '(1 2 3 4))) ; Debe mostrar "Satisfactible"
+(display (EVALUARSAT parsed-instance2 '(-1 2))) ; Debe mostrar "Satisfactible"
+(display (EVALUARSAT parsed-instance3 '(1))) ; Debe mostrar "Satisfactible"
+
